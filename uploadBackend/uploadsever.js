ff986ee5069f89410b11ae525b5e5b921ee05231
benchawan-bcw <benchawan.poo@spumail.net>
2025-02-24 19:45:54 +0700

    17

--- uploadBackend/uploadsever.js
+++ uploadBackend/uploadsever.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 import path from "path";
-import fs from "fs";
+import fs from "fs"
 
 const app = express();
 app.use(cors());
